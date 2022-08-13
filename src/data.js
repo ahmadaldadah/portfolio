e9048157fb68e06b92522179991171dd1a359ad0
ahmadaldadah <aldadahahmad@gmail.com>
2022-08-13 16:18:39 +0300

    deploy

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const projects = [
+    {
+      title: "React Color Palette",
+      subtitle: "React",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img/colors.png",
+      link: "https://colors-app-react.netlify.app/",
+    },
+    {
+      title: "React Admin Panel",
+      subtitle: "React",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img/AdminPanel.png",
+      link: "https://reactjs-admin-panel.netlify.app",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img/Homepage.png",
+      link: "https://home-page-social-media.netlify.app",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img/Todo-app.png",
+      link: "https://todo-app-ahmadaldadah.netlify.app",
+    },
+    {
+        title: "Landing Page Natours",
+        subtitle: "Html Css SCSS",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/landing-page-2.png",
+        link: "https://landing-page-natours.netlify.app",
+      },
+      {
+        title: "Landing Page Nexter",
+        subtitle: "Html Css SCSS",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/landing-page-1.png",
+        link: "https://landing-page-nexter.netlify.app",
+      },
+      {
+        title: "Landing Page Trillo",
+        subtitle: "Html Css SCSS",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/landing-page-3.png",
+        link: "https://landing-page-trillo.netlify.app",
+      },
+      {
+        title: "React Yahtzee Game",
+        subtitle: "React Hooks",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./img/Yahtzee.png",
+        link: "https://react-yahtzee-game-app.netlify.app",
+      },
+  ];
+export const skills = [
+    "JavaScript",
+    "React.js",
+    "Vue.js",
+    "Node",
+    "Material UI",
+    "Bootstrap",
+    "PHP",
+    "Laravel",
+    "Mysql",
+    "Html",
+    "Css",
+    "SCSS"
+];
